cfe029e0256c42149cee7cfed63aa605b803f217
daryazakharova <daryazakharova87@gmail.com>
2022-03-29 16:52:23 +0300

    hw29

---
+++ BlazorApp4/Data/User.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BlazorApp4.Data
+{
+    public class User
+    {
+        private static string _surname;
+        private static string _userName;
+        private static string _secondName;
+        private static string _login;
+
+        public User(string surnameName, string userName, string secondName, string login)
+        {
+            SurnameName = surnameName;
+            UserName = userName;
+            SecondName = secondName;
+            Login = login;
+        }
+
+        [BsonIgnoreIfDefault]
+        public ObjectId  Id { get; set; }
+        public string SurnameName { get => _surname; set => _surname = value; }
+        public string UserName { get => _userName; set => _userName = value; } 
+        public string SecondName { get => _secondName; set => _secondName = value; }
+        public string Login { get => _login; set => _login = value; }
+
+    }
+}
