49b8a05b6e53e0780fb31446d6de1c5da0256f7d
nacharvs <r3dinc@yandex.ru>
2022-03-30 19:22:43 +0300

    add login table

---
+++ BlazorApp4/Data/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp4.Data
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+        public static List<User> GetList()
+        {
+            return null;
+        }
+    }
+}
