24df9c701ebe2be0b3db930acfd0488004d20c9f
Petrikov Nikolay <petrikovnl@gmail.com>
2022-03-31 23:28:19 +0500

    my_Edit

--- BlazorApp4/Data/User.cs
+++ BlazorApp4/Data/User.cs
@@ -28,7 +28,6 @@ namespace BlazorApp4.Data
         public string SurName { get; set; }
         public string PhoneNumber { get; set; }
 
-
         public static List<User> GetList()
         {
             List<User> listToReturn = new List<User>();
