1a6f545e0e4e3fd78910066e33b0a38f13b4c05b
Petrikov Nikolay <petrikovnl@gmail.com>
2022-04-03 22:36:30 +0500

    03.04(end of day)

---
+++ BlazorApp4/Data/TaskItem.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Driver;
+
+namespace BlazorApp4.Data
+{
+    public class TaskItem
+    {
+        public TaskItem(string name)
+        {
+            Name = name;
+        }
+
+        public TaskItem(string name, bool isDone, bool inProgress) : this(name)
+        {
+            IsDone = isDone;
+            InProgress = inProgress;
+        }
+
+        private bool _isDone;
+        private bool _inProgress;
+        public string Name { get; set; }
+        public bool IsDone
+        {
+            get => _isDone;
+            set
+            {
+                if (_inProgress)
+                {
+                    _isDone = true;
+                    _inProgress = false;
+                }
+                else
+                {
+                    _isDone = true;
+                }
+            }
+        }
+        public bool InProgress {
+            get => _inProgress; 
+            set
+            {
+                if (_isDone)
+                {
+                    _isDone = false;
+                    _inProgress = true;
+                }
+                else
+                {
+                    _inProgress = true;
+                }
+            }
+        }
+    }
+}
---
+++ BlazorApp4/Data/TaskListDB.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace BlazorApp4.Data
+{
+    public class TaskListDB
+    {
+        public TaskListDB(List<TaskItem> taskList)
+        {
+            this.taskList = taskList;
+        }
+
+        public ObjectId _id { get; set; }
+        [BsonElement("ListOfTask")]
+        public List<TaskItem> taskList { get; set; }
+        public static void AddItem(TaskListDB item, string day)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("TaskList");
+            var collection = database.GetCollection<TaskListDB>(day);
+            collection.InsertOne(item);
+        }
+    }
+}
