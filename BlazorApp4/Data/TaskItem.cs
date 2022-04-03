4ae6f9cbeb6ac0c43286904bfe490604f210e0a1
akhmadullinaynur <akhmadullinaynur@yandex.ru>
2022-04-03 19:56:15 +0300

    fix

---
+++ BlazorApp4/Data/TaskItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp4.Data
+{
+    public class TaskItem
+    {
+        private bool _isDone;
+        private bool _isProgress;
+
+        public TaskItem(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public bool IsDone
+        {
+            get => _isDone;
+            set
+            {
+                _isDone = value;
+                _isProgress = false;
+            }
+        }
+
+        public bool IsProgress
+        {
+            get => _isProgress;
+            set
+            {
+                _isProgress = value;
+                _isDone = false;
+            }
+        }
+    }
+}
