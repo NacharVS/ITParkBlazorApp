7a814f02cb41dca1f043e28bf8be7adf3febf484
nacharvs <r3dinc@yandex.ru>
2022-04-03 19:04:27 +0300

    taskList

---
+++ BlazorApp4/Data/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp4.Data
+{
+    public class TaskItem
+    {
+        public TaskItem(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
