63c75135e63b9099d64f9020c030c63ca3f88302
olgaptrova <olgaptrova@gmail.com>
2022-04-07 17:23:32 +0300

    copy_32

---
+++ BlazorApp4/TaskItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp4.Data
+{
+    public class TaskItem
+    {
+        private bool _isDone;
+        private bool _inProgress;
+        public TaskItem(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public bool IsDone
+        {
+            get => _isDone;
+            set
+            {
+                _isDone = value;
+            }
+        }
+        public bool InProgress
+        {
+            get => _inProgress;
+            set
+            {
+                _inProgress = value;
+            }
+        }
+    }
+}
