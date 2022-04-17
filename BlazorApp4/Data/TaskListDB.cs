122a63c1042006822b91ec83119bb7df8c8d94e8
akhmadullinaynur <akhmadullinaynur@yandex.ru>
2022-04-17 16:33:53 +0300

    HW Button Colors

--- BlazorApp4/Data/TaskListDB.cs
+++ BlazorApp4/Data/TaskListDB.cs
@@ -4,6 +4,7 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BlazorApp4.Data
@@ -26,23 +27,42 @@ namespace BlazorApp4.Data
             var collection = database.GetCollection<TaskListDB>(day);
             if (database.ListCollectionNames().ToList().Exists(x => x == day))
             {
-                collection.ReplaceOne(x=>true, item);
+                collection.ReplaceOne(x => true, item);
             }
             else
-            collection.InsertOne(item);
+            {
+                collection.InsertOne(item);
+            }
+
         }
-
-
-        public static List<TaskItem> GetTaskList(string day)
+        public static async Task<List<TaskItem>> GetItem(string searchDay)
         {
             var client = new MongoClient("mongodb://localhost");
             var database = client.GetDatabase("TaskList");
-            var collection = database.GetCollection<TaskListDB>(day);
-            var TaskListFromDB = collection.Find(x => true).ToList();
-            List<TaskItem> listToReturn = new List<TaskItem>();
-            listToReturn.AddRange(collection.Find(x => true).FirstOrDefault().taskList);
-            return listToReturn;
-        }
+            if (database.ListCollectionNames().ToList().Exists(x => x == searchDay))
+            {
+                if (string.IsNullOrEmpty(searchDay))
+                {
+                    return null;
+                }
+                else
+                {
+                    var collection = database.GetCollection<TaskListDB>(searchDay);
+                    List<TaskListDB> document = await collection.Find(x => true).ToListAsync();
+                    //TaskListDB document = collection.Find(x => true).FirstOrDefault();
+                    var list = new List<TaskItem>();
+                    foreach (var item in document)
+                    {
+                        list.AddRange(item.taskList);
+                    }
+                    return list;
+                }
+            }
+            else
+            {
+                return null;
+            }
 
+        }
     }
 }
