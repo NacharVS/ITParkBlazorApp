38d2f6d07b536cf4de0754ffbbab2b6b020b8405
r3dinc <r3dinc@yandex.ru>
2022-05-05 20:07:03 +0300

    fix

---
+++ BlazorApp4/Sevises.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp4
+{
+    public class TransientService
+    {
+        public int Value { get; set; }
+    }
+
+    public class SingletonService
+    {
+        public int Value { get; set; }
+
+        public void Inc(ref int increment)
+        {
+            increment++;
+            Value = increment;
+
+        }
+    }
+}
