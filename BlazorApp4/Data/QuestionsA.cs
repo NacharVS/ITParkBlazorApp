f9a1ce8a3e2b88409a8f4e97a0e752fd6ee03ccb
nacharvs <r3dinc@yandex.ru>
2022-05-26 21:31:39 +0300

    testans

---
+++ BlazorApp4/Data/QuestionsA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp4.Data
+{
+    public class QuestionsA
+    {
+        public QuestionsA(string question, List<TestAnswers> myProperty)
+        {
+            Question = question;
+            MyProperty = myProperty;
+        }
+
+        public string Question { get; set; }
+        public List<TestAnswers> MyProperty { get; set; }
+
+        public static List<QuestionsA> GenerateQuestions()
+        {
+            List<QuestionsA> listToReturn = new List<QuestionsA>();
+            List<TestAnswers> anwers = new List<TestAnswers>();
+            anwers.Add(new TestAnswers("5", false));
+            anwers.Add(new TestAnswers("2", false));
+            anwers.Add(new TestAnswers("1", false)); 
+            anwers.Add(new TestAnswers("4", true));
+            listToReturn.Add(new QuestionsA("2+2 = ", anwers));
+            return listToReturn;
+        }
+    }
+}
---
+++ BlazorApp4/Data/TestAnswers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp4.Data
+{
+    public class TestAnswers
+    {
+        public TestAnswers(string possibleAnswer, bool isRight)
+        {
+            PossibleAnswer = possibleAnswer;
+            IsRight = isRight;
+        }
+
+        public string PossibleAnswer { get; set; }
+        public bool IsRight { get; set; }
+    }
+}
